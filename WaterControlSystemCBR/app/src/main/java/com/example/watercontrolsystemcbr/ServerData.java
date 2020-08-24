a27dd322cad33c72e5a8103f474b9e8c8750ccaa
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-24 20:15:53 +0400

    Update ServerData.java

--- WaterControlSystemCBR/app/src/main/java/com/example/watercontrolsystemcbr/ServerData.java
+++ WaterControlSystemCBR/app/src/main/java/com/example/watercontrolsystemcbr/ServerData.java
@@ -1,11 +1,11 @@
 package com.example.watercontrolsystemcbr;
 
 public class ServerData {
-    private static final String ROOT_URL = "http://moayad.eu5.org/Mobile_App_Server.php";
-    private static final String hostname = "localhost";
-    private static final String username  = "148523";
-    private static final String password = "Moayad258";
-    private static final String dbname  = "148523";
+    private static final String ROOT_URL = "";
+    private static final String hostname = "";
+    private static final String username  = "";
+    private static final String password = "";
+    private static final String dbname  = "";
     public String getRootUrl(){
         return ROOT_URL;
     }
