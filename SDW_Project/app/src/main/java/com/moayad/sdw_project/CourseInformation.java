91d6733f9ca56d921c1a38477adc4f344f7205c1
Moayadooh <moayadsalim97@gmail.com>
2024-08-29 07:44:28 +0400

    upgrade SDW_Project

--- SDW_Project/app/src/main/java/com/moayad/sdw_project/CourseInformation.java
+++ SDW_Project/app/src/main/java/com/moayad/sdw_project/CourseInformation.java
@@ -1,7 +1,7 @@
 package com.moayad.sdw_project;
 
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
