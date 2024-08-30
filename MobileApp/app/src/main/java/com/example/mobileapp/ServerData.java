229b6c4377260078c2416199a2a27bfdb2f8ea5b
Moayadooh <moayadsalim97@gmail.com>
2024-08-30 15:03:20 +0400

    upgrade the projects

--- MobileApp/app/src/main/java/com/example/mobileapp/ServerData.java
+++ MobileApp/app/src/main/java/com/example/mobileapp/ServerData.java
@@ -2,10 +2,10 @@ package com.example.mobileapp;
 
 public class ServerData {
     private static final String ROOT_URL = "";
-    private static final String hostname = "";
-    private static final String username  = "";
+    private static final String hostname = "localhost";
+    private static final String username  = "root";
     private static final String password = "";
-    private static final String dbname  = "";
+    private static final String dbname  = "palm";
     public String getRootUrl(){
         return ROOT_URL;
     }
